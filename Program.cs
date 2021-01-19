28fca886ffa87d7dd038b78570e22980ffd58e9c
Ryan <ryanstanford251@gmail.com>
2021-01-18 23:35:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodedMesssages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
