48f47d02be55f6f49a5b6aad8c2c56826f59a734
Ryan <ryanstanford251@gmail.com>
2021-01-19 23:33:02 -0600

    added encrypt method that changes each character in the entered message by the amount specified in the key

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace CodedMesssages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter message: ");
+            string plainText = Console.ReadLine();
+            if(int.TryParse(Console.ReadLine(), out int key))
+            encrypt(plainText, key);
+            
+
+        }
+
+        static void encrypt(string message, int key)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                int charToInt = (int)message[i];
+                char[] intToChar = new char[message.Length];
+                if (charToInt > 96 && charToInt < 123) //lowercase
+                {
+                    charToInt = ((int)message[i] - 96 + key % 26) + 96;
+                    intToChar[i] = (char)charToInt;
+                }
+                else if (charToInt > 64 && charToInt < 91) //uppercase
+                {
+                    charToInt = ((int)message[i] - 64 + key % 26) + 64;
+                    intToChar[i] = (char)charToInt;
+                }
+                else
+                {
+                    intToChar[i] = (char)charToInt;
+                }
+                
+            }
         }
     }
 }
