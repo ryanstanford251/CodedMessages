cffcaca20307db7bf8e59265f7d3b4b77349862a
Ryan <ryanstanford251@gmail.com>
2021-01-21 22:21:53 -0600

    added code to decrypt method to return an encrypted message back to plain text

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ namespace CodedMesssages
             if (int.TryParse(Console.ReadLine(), out int key)) { };
 
             string s = new string(encrypt(plainText, key));
-            Console.WriteLine(s);
+            Console.WriteLine($"Encrypted string {s}");
+
+            string x = new string(decrypt(s, 1));
+            Console.WriteLine($"plain text: {x}");
             
 
         }
@@ -41,9 +44,29 @@ namespace CodedMesssages
                 return intToChar;
         }
 
-        static void decrypt()
+        static char[] decrypt(string encryptedMessage, int key)           
         {
+            char[] intToChar = new char[encryptedMessage.Length];
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                int charToInt = (int)encryptedMessage[i];
+                if (charToInt > 96 && charToInt < 123) //lowercase
+                {
+                    charToInt = ((int)encryptedMessage[i] - 96 - key % 26) + 96;
+                    intToChar[i] = (char)charToInt;
+                }
+                else if (charToInt > 64 && charToInt < 91) //uppercase
+                {
+                    charToInt = ((int)encryptedMessage[i] - 64 - key % 26) + 64;
+                    intToChar[i] = (char)charToInt;
+                }
+                else
+                {
+                    intToChar[i] = (char)charToInt;
+                }
 
+            }
+            return intToChar;
         }
     }
 }
