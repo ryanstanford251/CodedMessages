24cd64fd91dc645034685079ac05217bdae61f6f
Ryan <ryanstanford251@gmail.com>
2021-01-28 23:05:15 -0600

    updated Main by adding ReadLine for key and a do while loop for isValidLetterShiftKey method

--- Program.cs
+++ Program.cs
@@ -7,29 +7,35 @@ namespace CodedMesssages
     {
         static void Main(string[] args)
         {
-            /*Console.Write("Enter message: ");
-            string plainText = Console.ReadLine();
-            Console.Write("Enter key: ");
-            if (int.TryParse(Console.ReadLine(), out int key)) { };
-
-            string s = new string(letterShiftEncryption(plainText, key));
-            Console.WriteLine($"Encrypted string {s}");
-
-            string x = new string(letterShiftDecryption(s, 1));
-            Console.WriteLine($"plain text: {x}");*/
-
-
-            Console.Write("plaintext: ");
-            string plainText = Console.ReadLine();
-            Console.Write("Key: ");
-            string key = Console.ReadLine();
-            if (isValidSubstitutionKey(key))
+            Console.Write("Enter message: ");
+            string message = Console.ReadLine();
+            int key;
+            do
             {
-                string result = new string(substitutionEncryption(key, plainText));
-                Console.WriteLine($" Encrypted Text: {result}");
-                string decryptedResult = new string(substitutionDecryption(key, result));
-                Console.WriteLine($"Decrypted Text: {decryptedResult}");
-            }
+                Console.Write("Enter encryption key: ");
+                key = isValidLetterShiftKey(Console.ReadLine());
+
+            } while (key < 1);
+            string encryptedMessage = letterShiftEncryption(message, key);
+            string decryptedMessage = letterShiftDecryption(encryptedMessage, key);
+            Console.WriteLine($"Plaintext Message: {message}");
+            Console.WriteLine($"Encrypted Message: {encryptedMessage}");
+            Console.WriteLine($"Decrypted Message: {decryptedMessage}");
+            
+            
+
+
+            //Console.Write("plaintext: ");
+            //string plainText = Console.ReadLine();
+            //Console.Write("Key: ");
+            //string key = Console.ReadLine();
+            //if (isValidSubstitutionKey(key))
+            //{
+            //    string result = new string(substitutionEncryption(key, plainText));
+            //    Console.WriteLine($" Encrypted Text: {result}");
+            //    string decryptedResult = new string(substitutionDecryption(key, result));
+            //    Console.WriteLine($"Decrypted Text: {decryptedResult}");
+            //}
             
             
 
